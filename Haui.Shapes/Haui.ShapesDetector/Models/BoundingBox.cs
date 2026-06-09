1b1591fd1622dc95dfa225d47753df95e3c97cc0
cmsang <sangcm.research@gmail.com>
2026-06-09 22:16:17 +0700

    chuyển nhà

--- Haui.Shapes/Haui.ShapesDetector/Models/BoundingBox.cs
+++
@@ -1,9 +0,0 @@
-namespace Haui.ShapesDetector.Models;
-
-public class BoundingBox
-{
-    public float X { get; set; }
-    public float Y { get; set; }
-    public float Width { get; set; }
-    public float Height { get; set; }
-}
--- Haui.Shapes/Haui.ShapesDetector/Models/DetectionResult.cs
+++
@@ -1,9 +0,0 @@
-namespace Haui.ShapesDetector.Models;
-
-public class DetectionResult
-{
-    public string ClassName { get; set; } = string.Empty;
-    public float Confidence { get; set; }
-    public BoundingBox BoundingBox { get; set; } = new();
-    public DateTime DetectedAt { get; set; } = DateTime.Now;
-}
